8d4ae2f878673206a74516bcb7ca51c76d90fd51
HarryLima007 <harrylimaamil@gmail.com>
2026-09-10 18:36:28 -0400

    Bust cache for smoother Pokedex outline

--- team-special-forms.js
+++ team-special-forms.js
@@ -194,7 +194,7 @@
 
   const homeStage2Style = document.createElement('link');
   homeStage2Style.rel = 'stylesheet';
-  homeStage2Style.href = 'home-stage2.css?v=home-stage2-center-split-trace-20260910-6';
+  homeStage2Style.href = 'home-stage2.css?v=home-stage2-smooth-split-trace-20260910-7';
   document.head.appendChild(homeStage2Style);
 
   const favFormsScript = document.createElement('script');
