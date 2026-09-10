ce37bfefc36bf4ae639c69ec28f41bb364edb6b6
HarryLima007 <harrylimaamil@gmail.com>
2026-09-10 18:28:30 -0400

    Bust home card trace animation cache

--- team-special-forms.js
+++ team-special-forms.js
@@ -194,7 +194,7 @@
 
   const homeStage2Style = document.createElement('link');
   homeStage2Style.rel = 'stylesheet';
-  homeStage2Style.href = 'home-stage2.css?v=home-stage2-outline-test-20260910-4';
+  homeStage2Style.href = 'home-stage2.css?v=home-stage2-trace-path-20260910-5';
   document.head.appendChild(homeStage2Style);
 
   const favFormsScript = document.createElement('script');
